de8c90b76b3c9a93372c8179de8da58180909fcc
Abby Kuranz <akuranz@gmail.com>
2019-12-01 14:52:35 -0700

    tried using a for loop - commented out code

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 # WeatherDashboard
+
+## Description
+
+Weather Dashboard based on city search using OpenWeather Map APIs. Current weaather forcast displays city name, date, weather condition icon, temperature, humidity, wind speed, and UV index. 5-day weather forecast displays date, weather condition icon, tempurature and humidity.
+
+## Instructions for User
+
+Search for any city in the search box and click enter. The current weather conditions and a five-day forecast will be displayed for the city. The search will also be saved and displayed in the search history below the search box.
+
+## HTML
+
+Most of the html is greated dynamically in JQuery.
+
+## Javascript
+
+The JQuery uses three separate AJAX calls and the OpenWeather Map APis for Current Weather Data and the 5 day / 3 hour forecast. It relies on looping thorugh an object and storing information in local storage. Moment.js is used for the dates.
+
+## APIs
+
+Current weather data API:
+https://openweathermap.org/current
+
+5 day weather forecast:
+https://openweathermap.org/forecast
+
+## Licensing
+
+This project uses one Font Awesome icon:
+
+- search is a free Font Awesome icon with Attribution 4.0 International (CC BY 4.0).
+
+MIT License
+
+Copyright (c) [2019][abby kuranz]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -29,12 +29,12 @@ function displayCurrentWeatherInfo() {
     );
     var kelvin = response.main.temp;
     var fahrenheit = ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
-    $(".current-temp").text("Temperature: " + fahrenheit + " F");
+    $(".current-temp").html("Temperature: " + fahrenheit + " &deg;F");
     $(".current-humid").text("Humidity: " + response.main.humidity + " %");
     $(".current-wind").text("Wind Speed: " + response.wind.speed + " MPH");
 
     var queryURLuvi =
-      "http://api.openweathermap.org/data/2.5/uvi?appid=" +
+      "https://api.openweathermap.org/data/2.5/uvi?appid=" +
       APIKey +
       "&lat=" +
       response.coord.lat +
@@ -79,204 +79,211 @@ function displayFiveDayWeatherForecast() {
     method: "GET"
   }).then(function(response) {
     console.log(response);
-    var days = response.list;
-    var fiveDayList = 0;
+    //Not Working
+    // var days = response.list;
+    // var fiveDayList = 0;
 
-    for (var i = 0; i < days.length && fiveDayList < 5; i++) {
-      var timeString = days[i].dt_txt.split(" ")[1].split(":")[0];
-      if (timeString === "18") {
-        fiveDayList++;
-        console.log(fiveDayList);
-        var div = $("<div class='col-md-2'>").html(ForecastCard(days[i]));
-        $("#forecast-row").append(div);
+    // for (var i = 0; i < days.length && fiveDayList < 5; i++) {
+    //   console.log(fiveDayList);
 
-        var icon = $("<img>").attr(
-          "src",
-          "http://openweathermap.org/img/wn/" +
-            days[i].weather[0].icon +
-            "@2x.png"
-        );
+    //   var timeString = days[i].dt_txt.split(" ")[1].split(":")[0];
+    //   console.log(timeString);
 
-        var currentDate = days[i].dt_txt;
-        var currentDateFormatted = moment(currentDate).format("MM/DD/YYYY");
-        console.log("Current Date:", currentDate);
-        console.log("Date of Index:", moment(currentDate).format("MM/DD/YYYY"));
+    //   if (timeString === "18") {
+    //     fiveDayList++;
+    //     console.log(fiveDayList);
 
-        function ForecastCard(days) {
-          var obj = {
-            date: currentDateFormatted,
-            img: icon,
-            temp: "hot",
-            humidity: "20%"
-          };
+    //     var currentDate = days[i].dt_txt;
+    //     var currentDateFormatted = moment(currentDate).format("MM/DD/YYYY");
+    //     console.log("Current Date:", currentDate);
+    //     console.log("Date of Index:", moment(currentDate).format("MM/DD/YYYY"));
 
-          return `<div class="card forecast">
-            <div class="card-body">
-              <h5 id="date-day-1">${obj.date}</h5>
-              <div><img id="icon-day-1"/>${obj.img}</div>
-              <div id="temp-day-1"> ${obj.temp} </div>
-              <div id="humid-day-1">${obj.humidity}</div>
-            </div>
-          </div>`;
-        }
+    //     var kelvinForecast = days[i].main.temp;
+    //     var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(
+    //       0
+    //     );
 
-        // //Forecast Dates
-        // $("#date-day-1").text(moment(currentDate).format("MM/DD/YYYY"));
-        // $("#date-day-2").text(
-        //   moment(currentDate)
-        //     .add(1, "days")
-        //     .format("MM/DD/YYYY")
-        // );
-        // $("#date-day-3").text(
-        //   moment(currentDate)
-        //     .add(2, "days")
-        //     .format("MM/DD/YYYY")
-        // );
-        // $("#date-day-4").text(
-        //   moment(currentDate)
-        //     .add(3, "days")
-        //     .format("MM/DD/YYYY")
-        // );
-        // $("#date-day-5").text(
-        //   moment(currentDate)
-        //     .add(4, "days")
-        //     .format("MM/DD/YYYY")
-        // );
+    //     var avgHumidity = days[i].main.humidity;
 
-        // console.log(
-        //   "Date of Index + 1:",
-        //   moment(currentDate)
-        //     .add(1, "days")
-        //     .format("MM/DD/YYYY")
-        // );
+    //     var div = $("<div class='col-md-2'>").html(ForecastCard(days[i]));
+    //     $("#forecast-row").append(div);
 
-        //Forecast Icons
-        // $("#icon-day-1").attr(
-        //   "src",
-        //   "http://openweathermap.org/img/wn/" +
-        //     days[i].weather[0].icon +
-        //     "@2x.png"
-        // );
-        // $("#icon-day-2").attr(
-        //   "src",
-        //   "http://openweathermap.org/img/wn/" +
-        //     days[i].weather[0].icon +
-        //     "@2x.png"
-        // );
-        // $("#icon-day-3").attr(
-        //   "src",
-        //   "http://openweathermap.org/img/wn/" +
-        //     days[i].weather[0].icon +
-        //     "@2x.png"
-        // );
-        // $("#icon-day-4").attr(
-        //   "src",
-        //   "http://openweathermap.org/img/wn/" +
-        //     days[i].weather[0].icon +
-        //     "@2x.png"
-        // );
-        // $("#icon-day-5").attr(
-        //   "src",
-        //   "http://openweathermap.org/img/wn/" +
-        //     days[i].weather[0].icon +
-        //     "@2x.png"
-        // );
+    //     var icon = $("<img>").attr(
+    //       "src",
+    //       "http://openweathermap.org/img/wn/" +
+    //         days[i].weather[0].icon +
+    //         "@2x.png"
+    //     );
+    //     console.log(icon[0]);
 
-        // for (var i = 0; i < response.list.length; i++) {
-        //   console.log("noon", response.list[i].dt_txt.includes("12:00:00"));
-        //   var noonIndex = response.list[i].dt_text
-        //     .includes("12:00:00")
-        //     .parent()
-        //     .index();
-        //   console.log(noonIndex);
-        // }
+    //     var iconImage = icon[0];
 
-        // for (let key of response.list) {
-        //   console.log("date", $(this).dt_txt);
-        // }
+    //     function ForecastCard() {
+    //       var obj = {
+    //         date: currentDateFormatted,
+    //         img: iconImage,
+    //         temp: "Temperature: " + fahrenheitForecast + " &deg;F",
+    //         humidity: "Humidity: " + avgHumidity + "%"
+    //       };
 
-        // Forecast temp
-        var kelvinForecast =
-          (response.list[5].main.temp +
-            response.list[6].main.temp +
-            response.list[7].main.temp) /
-          3;
-        var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(
-          0
-        );
-        $("#temp-day-1").text("Temperature: " + fahrenheitForecast + " F");
-        var kelvinForecast =
-          (response.list[13].main.temp +
-            response.list[14].main.temp +
-            response.list[15].main.temp) /
-          3;
-        var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(
-          0
-        );
-        $("#temp-day-2").text("Temperature: " + fahrenheitForecast + " F");
-        var kelvinForecast =
-          (response.list[21].main.temp +
-            response.list[22].main.temp +
-            response.list[23].main.temp) /
-          3;
-        var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(
-          0
-        );
-        $("#temp-day-3").text("Temperature: " + fahrenheitForecast + " F");
-        var kelvinForecast =
-          (response.list[29].main.temp +
-            response.list[30].main.temp +
-            response.list[31].main.temp) /
-          3;
+    //       return `<div class="card forecast">
+    //         <div class="card-body">
+    //           <h5>${obj.date}</h5>
+    //           <div>${obj.img}</div>
+    //           <div> ${obj.temp} </div>
+    //           <div>${obj.humidity}</div>
+    //         </div>
+    //       </div>`;
+    //     }
+    //   }
 
-        var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(
-          0
-        );
-        $("#temp-day-4").text("Temperature: " + fahrenheitForecast + " F");
-        var kelvinForecast =
-          (response.list[37].main.temp +
-            response.list[38].main.temp +
-            response.list[39].main.temp) /
-          3;
+    //Forecast Dates
 
-        var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(
-          0
-        );
-        $("#temp-day-5").text("Temperature: " + fahrenheitForecast + " F");
+    var currentDate = response.list[0].dt_txt;
+    $("#date-day-1").text(moment(currentDate).format("MM/DD/YYYY"));
+    $("#date-day-2").text(
+      moment(currentDate)
+        .add(1, "days")
+        .format("MM/DD/YYYY")
+    );
+    $("#date-day-3").text(
+      moment(currentDate)
+        .add(2, "days")
+        .format("MM/DD/YYYY")
+    );
+    $("#date-day-4").text(
+      moment(currentDate)
+        .add(3, "days")
+        .format("MM/DD/YYYY")
+    );
+    $("#date-day-5").text(
+      moment(currentDate)
+        .add(4, "days")
+        .format("MM/DD/YYYY")
+    );
 
-        //Forcast Humidity
-        var avgHumidity =
-          (response.list[5].main.humidity +
-            response.list[6].main.humidity +
-            response.list[7].main.humidity) /
-          3;
-        $("#humid-day-1").text("Humidity: " + avgHumidity.toFixed(0) + " %");
-        var avgHumidity =
-          (response.list[13].main.humidity +
-            response.list[14].main.humidity +
-            response.list[15].main.humidity) /
-          3;
-        $("#humid-day-2").text("Humidity: " + avgHumidity.toFixed(0) + " %");
-        var avgHumidity =
-          (response.list[21].main.humidity +
-            response.list[22].main.humidity +
-            response.list[23].main.humidity) /
-          3;
-        $("#humid-day-3").text("Humidity: " + avgHumidity.toFixed(0) + " %");
-        var avgHumidity =
-          (response.list[29].main.humidity +
-            response.list[30].main.humidity +
-            response.list[31].main.humidity) /
-          3;
-        $("#humid-day-4").text("Humidity: " + avgHumidity.toFixed(0) + " %");
-        var avgHumidity =
-          (response.list[37].main.humidity +
-            response.list[38].main.humidity +
-            response.list[39].main.humidity) /
-          3;
-        $("#humid-day-5").text("Humidity: " + avgHumidity.toFixed(0) + " %");
-      }
-    }
+    console.log(
+      "Date of Index + 1:",
+      moment(currentDate)
+        .add(1, "days")
+        .format("MM/DD/YYYY")
+    );
+
+    //Forecast Icons
+    $("#icon-day-1").attr(
+      "src",
+      "http://openweathermap.org/img/wn/" +
+        response.list[6].weather[0].icon +
+        "@2x.png"
+    );
+    $("#icon-day-2").attr(
+      "src",
+      "http://openweathermap.org/img/wn/" +
+        response.list[14].weather[0].icon +
+        "@2x.png"
+    );
+    $("#icon-day-3").attr(
+      "src",
+      "http://openweathermap.org/img/wn/" +
+        response.list[22].weather[0].icon +
+        "@2x.png"
+    );
+    $("#icon-day-4").attr(
+      "src",
+      "http://openweathermap.org/img/wn/" +
+        response.list[30].weather[0].icon +
+        "@2x.png"
+    );
+    $("#icon-day-5").attr(
+      "src",
+      "http://openweathermap.org/img/wn/" +
+        response.list[38].weather[0].icon +
+        "@2x.png"
+    );
+
+    // for (var i = 0; i < response.list.length; i++) {
+    //   console.log("noon", response.list[i].dt_txt.includes("12:00:00"));
+    //   var noonIndex = response.list[i].dt_text
+    //     .includes("12:00:00")
+    //     .parent()
+    //     .index();
+    //   console.log(noonIndex);
+    // }
+
+    // for (let key of response.list) {
+    //   console.log("date", $(this).dt_txt);
+    // }
+
+    // Forecast temp
+    var kelvinForecast =
+      (response.list[5].main.temp +
+        response.list[6].main.temp +
+        response.list[7].main.temp) /
+      3;
+    var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(0);
+    $("#temp-day-1").text("Temperature: " + fahrenheitForecast + " F");
+    var kelvinForecast =
+      (response.list[13].main.temp +
+        response.list[14].main.temp +
+        response.list[15].main.temp) /
+      3;
+    var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(0);
+    $("#temp-day-2").text("Temperature: " + fahrenheitForecast + " F");
+    var kelvinForecast =
+      (response.list[21].main.temp +
+        response.list[22].main.temp +
+        response.list[23].main.temp) /
+      3;
+    var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(0);
+    $("#temp-day-3").text("Temperature: " + fahrenheitForecast + " F");
+    var kelvinForecast =
+      (response.list[29].main.temp +
+        response.list[30].main.temp +
+        response.list[31].main.temp) /
+      3;
+
+    var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(0);
+    $("#temp-day-4").text("Temperature: " + fahrenheitForecast + " F");
+    var kelvinForecast =
+      (response.list[37].main.temp +
+        response.list[38].main.temp +
+        response.list[39].main.temp) /
+      3;
+
+    var fahrenheitForecast = ((kelvinForecast - 273.15) * 1.8 + 32).toFixed(0);
+    $("#temp-day-5").text("Temperature: " + fahrenheitForecast + " F");
+
+    //Forcast Humidity
+    var avgHumidity =
+      (response.list[5].main.humidity +
+        response.list[6].main.humidity +
+        response.list[7].main.humidity) /
+      3;
+    $("#humid-day-1").text("Humidity: " + avgHumidity.toFixed(0) + " %");
+    var avgHumidity =
+      (response.list[13].main.humidity +
+        response.list[14].main.humidity +
+        response.list[15].main.humidity) /
+      3;
+    $("#humid-day-2").text("Humidity: " + avgHumidity.toFixed(0) + " %");
+    var avgHumidity =
+      (response.list[21].main.humidity +
+        response.list[22].main.humidity +
+        response.list[23].main.humidity) /
+      3;
+    $("#humid-day-3").text("Humidity: " + avgHumidity.toFixed(0) + " %");
+    var avgHumidity =
+      (response.list[29].main.humidity +
+        response.list[30].main.humidity +
+        response.list[31].main.humidity) /
+      3;
+    $("#humid-day-4").text("Humidity: " + avgHumidity.toFixed(0) + " %");
+    var avgHumidity =
+      (response.list[37].main.humidity +
+        response.list[38].main.humidity +
+        response.list[39].main.humidity) /
+      3;
+    $("#humid-day-5").text("Humidity: " + avgHumidity.toFixed(0) + " %");
   });
 }
 
